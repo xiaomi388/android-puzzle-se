8c34f24a3b282bf59d1c74cd902afb5197c89c01
xiaomi388 <xiaomi388@gmail.com>
2018-07-01 14:09:30 +0800

    增加了一些注释

--- src/handlers/User.cpp
+++ src/handlers/User.cpp
@@ -13,16 +13,23 @@ UserHandler::UserHandler(const crow::request &r) : BaseController(r) {}
 
 crow::response UserHandler::Get() {
   // This is for test, and a template, please don't edit it.
+  // This interface is to get user info by username.
   vector<json> content;
   auto username = this->get_argument("username");
 
-  // Just for debug.
+  // Just for debug, you can use get_current_user_id() to
+  // get the current login user's id.
   if (this->get_current_user_id().empty()) {
     cout << "当前用户没有登录" << endl;
   } else {
     cout << this->get_current_user_id() << endl;
   }
 
+  // Find user info from database.
+  // If succeed, set a cookie to the current user.
+  // This is a meaningless action, just in order to 
+  // show how to connect to the database and set
+  // cookie for user.
   mysqlpp::ScopedConnection conn(*pool);
   mysqlpp::Query query = conn->query(fmt::format(
       "select * from user where username = {}", escapeSQL(username)));
@@ -34,7 +41,7 @@ crow::response UserHandler::Get() {
         "password": "%s"
       })", row[0], row[1], row[2]));
       content.push_back(rec);
-      ctx.set_cookie("uid", string(row[0]));
+      ctx.set_secure_cookie("uid", string(row[0]));
     }
   }
   return return_json("", content);
