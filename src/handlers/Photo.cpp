37390dfc680f6d2d8a29ab4a210c1f9ba74bb1a4
FredaHu <fredyqiu@hotmail.com>
2018-07-01 20:18:23 +0800

    finish photo

--- src/handlers/Photo.cpp
+++ src/handlers/Photo.cpp
@@ -11,7 +11,19 @@ using namespace PuzzleServer;
 PhotoHandler::PhotoHandler(const crow::request &r) : BaseController(r) {}
 
 crow::response PhotoHandler::Get() {
-  return return_json("");
+  vector<json> content;
+  mysqlpp::ScopedConnection conn(*pool);
+  mysqlpp::Query query = conn->query("select pid, src from photo");
+  if (mysqlpp::StoreQueryResult res = query.store()) {
+    for (auto &row : res) {
+      json rec = json::parse(fmt::sprintf(R"({
+        "pid": "%s",
+        "src": "%s"
+      })", row[0], row[1]));
+      content.push_back(rec);
+    }
+  }
+  return return_json("", content);
 }
 
 crow::response PhotoHandler::Post() {
